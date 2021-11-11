e11f50f98d161a03a5c7946dbb4a1b8a8982bfcc
adsionli <adsionli@foxmail.com>
2021-11-11 08:50:41 +0800

    webpack第四阶段学习开始

---
+++ webpack_04/src/main.js
@@ -0,0 +1,13 @@
+const title = "adsionli"
+
+const foo = () => {
+    console.log(title)
+}
+
+foo()
+
+const promise = new Promise((resolve, reject) => {
+    console.log(123)
+})
+
+console.log(promise)
---
+++ webpack_04/webpack.config.js
@@ -0,0 +1,110 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { DefinePlugin } = require('webpack')
+const CopyPlugin = require('copy-webpack-plugin')
+const webpack = require('webpack')
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    target: "web",
+    //如果开启在node api中，这里就不要配置devServer，将配置写在启动Node服务的地方
+    // devServer: {
+    //     hot: true
+    // },
+    optimization: {
+        moduleIds: 'named'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1,
+                            esModule: false
+                        }
+                    },
+                    'postcss-loader'
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1,
+                            esModule: false
+                        }
+                    },
+                    'postcss-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(jpe?g|git|png|svg)$/,
+                type: 'asset',
+                generator: {
+                    filename: 'img/[name][hash:4].[ext]'
+                },
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 30 * 1024
+                    }
+                }
+            },
+            {
+                test: /\.(ttf|woff2?)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'font/[name][hash:4].[ext]'
+                }
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                    }
+                ]
+            },
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin({
+            verbose: true,
+            CleanStaleWebPackageAssets: true
+        }),
+        new HtmlWebpackPlugin({
+            title: 'adsionli的webpack学习之路',
+            template: './public/index.html'
+        }),
+        new DefinePlugin({
+            BASE_URL: '"./"'
+        }),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: 'public',
+                    globOptions: {
+                        ignore: [
+                            '**/index.html'
+                        ]
+                    }
+                }
+            ]
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ]
+}
